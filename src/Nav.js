96291e90ab5ec8bdad6d1b44f65b70f763063fb5
Errol Rochester <rochestererrol@gmail.com>
2021-03-15 02:13:03 -0400

    navbar changes and trailer component added

--- src/Nav.js
+++ src/Nav.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import classes from './Nav.module.css';
 const Nav = (props) => {
+    const [show, handleShow] = useState(false);
+    useEffect(() => {
+        window.addEventListener("scroll", () => {
+            if(window.scrollY > 100){
+                handleShow(true);
+            } else{
+                handleShow(false);
+            }
+        });
+        return () => {
+            window.removeEventListener("scroll");
+        };
+    }, []);
+
     return ( 
-        <div>
+        <div className={show ? `${classes.nav} ${classes.nav_black}` : classes.nav}>
             <img 
-                className={classes.nav}
+                className={classes.nav_logo}
                 src='https://image.tmdb.org/t/p/original/wwemzKWzjKYJFfCeiB57q3r4Bcm.svg'
                 alt='Netflix Logo'
             />
--- src/Row.js
+++ src/Row.js
@@ -2,10 +2,15 @@
 import React, {useState, useEffect} from 'react';
 import axios from './axios';
 import classes from './Row.module.css';
+import Youtube from 'react-youtube';
+import movieTrailer from 'movie-trailer';
 const BASE_URL = "https://image.tmdb.org/t/p/original/";
+
 
 const Row = (props) => {
     const [movies, setMovies] = useState([]);
+    const [trailerUrl, setTrailerUrl] = useState("");
+
     useEffect(() => {
         //if [] run once when row loads and don't run again
         async function fetchData(){
@@ -17,15 +22,36 @@ const Row = (props) => {
         fetchData();
     }, [props.fetchUrl]);
 
+    const opts = {
+        height: "390",
+        width: "100%",
+        playerVars: {
+            autoplay: 1
+        }
+    }
     console.log(movies);
+
+    const handleClick = (movie) => {
+        if(trailerUrl){
+            setTrailerUrl('');
+        } else {
+            movieTrailer(movie?.name || "")
+                .then(url => {
+                    const urlParams = new URLSearchParams(new URL(url).search);
+                    setTrailerUrl(urlParams.get('v'));
+                })
+                .catch((error) => {console.log(error)});
+        }
+    }
     return ( 
         <div className={classes.row}>
             <h2>{props.title}</h2>
             <div className={classes.row_posters}>
                 {movies.map(movie => {
-                    return <img key={movie.id} className= {props.isLargeRow ? `${classes.row_poster} ${classes.row_posterLarge}` : classes.row_poster} src={`${BASE_URL}${props.isLargeRow ? movie.poster_path : movie.backdrop_path}`} alt={movie.name} />
+                    return <img key={movie.id} onClick={() => handleClick(movie)} className= {props.isLargeRow ? `${classes.row_poster} ${classes.row_posterLarge}` : classes.row_poster} src={`${BASE_URL}${props.isLargeRow ? movie.poster_path : movie.backdrop_path}`} alt={movie.name} />
                 })}
             </div>
+            {trailerUrl && <Youtube videoId={trailerUrl} opts={opts} />}
         </div>
      );
 }
